4547eca755a460703b1abd638ba402e634ec9f12
rednand <nan-aguiar@hotmail.com>
2022-03-23 15:54:03 -0300

    starting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from "react";
+import { AppContainer } from "./styles";
+const Buton: React.CSSProperties = {
+  backgroundColor: "red",
+};
+export const App = () => {
+  return <AppContainer>Columns will go here</AppContainer>;
+};
